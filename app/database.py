39cb15acc7f835d2043f72d790791c1d8065decd
Forgist7 <7forgist7@gmail.com>
2026-02-19 12:01:45 +0300

    remove default db url

--- app/database.py
+++ app/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:postgres@db:5432/tasks"
+    "DATABASE_URL"
+    # "postgresql://postgres:postgres@db:5432/tasks"
 )
 
 engine = create_engine(DATABASE_URL)
